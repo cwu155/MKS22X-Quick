126d6a62ff229f341fb1362adc67cf0ee1adad04
cwu155 <cwu17@stuy.edu>
2019-03-15 02:29:32 -0400

    Quickselect works?

--- Quick.java
+++ Quick.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Quick{
+  public static int ans;
 
   /*Modify the data array such that:
    *1. Only the indices from start to end inclusive are considered in range
@@ -43,11 +44,25 @@ public class Quick{
     return start;
   }
 
+  public static int quickselect(int[]data, int k){
+    partition(data, 0, data.length-1);
+    for (int i = 0; i < data.length; i++){
+      if (i == k){
+        ans = data[k];
+      }
+    }
+    return ans;
+  }
+
+
+
 
 
   public static void main(String[] args) {
-    int[] test = new int[]{999,999,999,4,1,0,3,2,999,999,999};
-    System.out.println(partition(test, 0, 10));
+    int[] test = new int[]{10,31, 10, 6,4 ,12, 29 };
+    System.out.println(partition(test, 0, 6));
     for (int element : test){System.out.println(element);}
+    System.out.println("\n");
+    System.out.println(quickselect(test, 3));
   }
 }
