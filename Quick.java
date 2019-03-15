8d7f7f4fadf91045439a4ec273bcfb60ad0d6540
cwu155 <cwu17@stuy.edu>
2019-03-15 03:15:28 -0400

    stack overflow errors :3

--- Quick.java
+++ Quick.java
@@ -39,10 +39,10 @@ public class Quick{
         end -= 1;
       }
     }
-
-    System.out.println("pivot: " + pivot);
     return start;
   }
+
+  /*return the value that is the kth smallest value of the array. k=0 is the smallest*/
 
   public static int quickselect(int[]data, int k){
     partition(data, 0, data.length-1);
@@ -55,14 +55,55 @@ public class Quick{
   }
 
 
+//I don't know how to write quickSort without a helper?? :(
+public static void quicksortH(int[] data, int start, int end) {
+    if (end - start <= 0) {
+       return;
+    } else {
+      int split = partition(data, start, end);
+      quicksortH(data, start, split- 1);
+      quicksortH(data, split + 1, end);
+    }
+}
 
+
+public static void quicksort(int[] data) {
+  quicksortH(data, 0, data.length - 1);
+}
 
 
   public static void main(String[] args) {
-    int[] test = new int[]{10,31, 10, 6,4 ,12, 29 };
-    System.out.println(partition(test, 0, 6));
-    for (int element : test){System.out.println(element);}
-    System.out.println("\n");
-    System.out.println(quickselect(test, 3));
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Quick.quicksort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
   }
+}
 }
