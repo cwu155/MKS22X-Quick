88248985243483fb8ba1c980704e30dcfd875708
cwu155 <cwu17@stuy.edu>
2019-03-18 14:49:21 -0400

    Tested splitting array into half

--- Quick.java
+++ Quick.java
@@ -18,8 +18,10 @@ public class Quick{
     Random rand = new Random();
     int index = rand.nextInt(data.length);
     int pivot = data[index];
+    data[index] = data[0];
+    data[0] = pivot;
 
-    while (start <= end){
+    while (start + 1 < end){
 
       while (data[start] < pivot){
         start += 1;
@@ -29,16 +31,16 @@ public class Quick{
         end -= 1;
       }
 
-      if (start >= end){
-        break;
-      } else if (start < end){
+      if (start < end){
         int temp = data[start];
         data[start] = data[end];
         data[end] = temp;
+
         start += 1;
         end -= 1;
       }
     }
+    System.out.println("pivot: " + pivot);
     return start;
   }
 
@@ -57,9 +59,7 @@ public class Quick{
 
 //I don't know how to write quickSort without a helper?? :(
 public static void quicksortH(int[] data, int start, int end) {
-    if (end - start <= 0) {
-       return;
-    } else {
+    if (!(end - start <= 0)) {
       int split = partition(data, start, end);
       quicksortH(data, start, split- 1);
       quicksortH(data, split + 1, end);
@@ -73,37 +73,9 @@ public static void quicksort(int[] data) {
 
 
   public static void main(String[] args) {
-    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-  int[]MAX_LIST = {1000000000,500,10};
-  for(int MAX : MAX_LIST){
-    for(int size = 31250; size < 2000001; size*=2){
-      long qtime=0;
-      long btime=0;
-      //average of 5 sorts.
-      for(int trial = 0 ; trial <=5; trial++){
-        int []data1 = new int[size];
-        int []data2 = new int[size];
-        for(int i = 0; i < data1.length; i++){
-          data1[i] = (int)(Math.random()*MAX);
-          data2[i] = data1[i];
-        }
-        long t1,t2;
-        t1 = System.currentTimeMillis();
-        Quick.quicksort(data2);
-        t2 = System.currentTimeMillis();
-        qtime += t2 - t1;
-        t1 = System.currentTimeMillis();
-        Arrays.sort(data1);
-        t2 = System.currentTimeMillis();
-        btime+= t2 - t1;
-        if(!Arrays.equals(data1,data2)){
-          System.out.println("FAIL TO SORT!");
-          System.exit(0);
-        }
-      }
-      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-    }
-    System.out.println();
+    int[] test = new int[]{10, 6,4 ,12, 13, 1, 22, 19};
+    //quicksort(test);
+    partition(test, 0, 7);
+    for (int e : test){System.out.println(e);}
   }
-}
 }
